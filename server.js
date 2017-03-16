0c8309170e52079700040a7a20a4ff73064b583e
Sean Pollock <seanpollock3344@gmail.com>
2017-03-15 19:47:06 -0500

    Adding in express server to host html and js

---
+++ server.js
@@ -0,0 +1,12 @@
+var express =  require('express');
+var path = require('path');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+})
+
+app.listen('5555', function() {
+  console.log('express server listening on port 5555');
+})
